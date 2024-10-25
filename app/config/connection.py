480943e6ac7de01e2f7baf4b36c877603390c52a
Wagner Silva <156625511+waguinhuu@users.noreply.github.com>
2024-10-25 12:41:38 +0000

    criando a conexão com banco de dados

--- app/config/connection.py
+++ app/config/connection.py
@@ -7,4 +7,12 @@ db_user = "user"
 db_password = "user_password"
 db_host = "localhost"
 db_port = "3306"
-db_name = "meu_ban
+db_name = "meu_banco"
+
+# URL da conexão para o banco de dados mySQL.
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+# Conectando ao banco de dados.
+db = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=db)
+session = Session()
