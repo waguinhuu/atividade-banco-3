bb5a0f720f6613793fce9fd6bef3a1892c9c0ced
Wagner Silva <156625511+waguinhuu@users.noreply.github.com>
2024-10-25 12:52:24 +0000

    criando a função salvar_usuario

--- app/repositores/usuario_repositories.py
+++ app/repositores/usuario_repositories.py
@@ -4,3 +4,8 @@ from sqlalchemy.orm import Session
 class UsuarioRepository:
     def __init__(self, session: Session):
         self.session = session
+    
+    def salvar_usuario(self, usuario: Usuario):
+        self.session.add(usuario)
+        self.session.commit()
+        self.session.refresh()
